b3fe9188842d2e7b3f0e1217324d787c38b9c7e1
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-07-30 20:23:34 +0400

    xd

--- tests/net_test.cpp
+++ tests/net_test.cpp
@@ -12,7 +12,7 @@ int main()
         net::ClientSocket<net::AddressFamily::IPV4, net::SocketType::STREAM, net::SocketProtocol::TCP> socket;
 
         socket.connect(net::lookup<net::AddressFamily::IPV4>("x.com")[0].addr, 80);
-        socket.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n", 37);
+        socket.send("GET / HTTP/1.1\r\nHost: x.com\r\n\r\n", 32);
         auto [data, size_recved] = socket.recv<char, 1024>();
 
         std::cout << std::string(data, size_recved) << std::endl;
